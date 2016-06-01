b7e138410bee6f8c278b0bf2b528b9d788eb0c53
KellerJordan <kellerjordan1998@gmail.com>
2016-05-31 19:23:58 -0700

    added node selection, relation selection, image folder

--- reader.php
+++ reader.php
@@ -1,18 +1,22 @@
 <?php
 
-function getNode($subid,$level){
+function getNode($relid,$subid,$level){
     global $con;
-    $return=["id"=>$subid,"relations"=>array()];
-    foreach($con->query("SELECT objid,relation FROM synapses WHERE subid='$subid' ORDER BY primarity") as $row){
-        // $return["relations"]["relid"]=$row["relid"];
+    $return=["relid"=>$relid,"id"=>$subid,"relations"=>array()];
+    foreach($con->query("SELECT relid,objid,relation FROM synapses WHERE subid='$subid' ORDER BY primarity") as $row){
         if(!array_key_exists($row["relation"],$return["relations"])){
             $return["relations"][$row["relation"]]=array();
         }
-        if($level>0&&$subid>=10){
-            array_push($return["relations"][$row["relation"]],getNode($row["objid"],$level-1));
+        if($level>0&&$subid>=100){
+            array_push($return["relations"][$row["relation"]],getNode($row["relid"],$row["objid"],$level-1));
         }
     }
     return $return;
+}
+
+function getRelation($relid){
+    global $con;
+    
 }
 
 $con=mysqli_connect("localhost","root","","synapsedb");
@@ -34,7 +38,13 @@ switch($_POST["type"]){
     break;
 }
 
-echo json_encode(getNode($subid,10));
+$deepness=10;
+if($_POST["type"]=="get_rel"){
+    echo json_encode(getRelation($subid));
+}else{
+    echo json_encode(getNode(null,$subid,$deepness));
+}
+
 mysqli_close($con);
 
 ?>
--- script.js
+++ script.js
@@ -6,6 +6,8 @@ var sWidth=626;
 var sHeight=500;
 
 var headings,branches,bubbles,nodeData;
+var selected=[];
+var selectTimeout=true;
 
 function query(type,subid,objid,relation){
     root=subid;
@@ -18,11 +20,24 @@ function query(type,subid,objid,relation){
         nodeData=JSON.parse(data);
         constructPage();    
         if(type=="del"){query("get",oroot);}
+        if(type=="get"){selectNode(subid.toString());}
     });
 }
 
+function selectNode(id){
+    selectTimeout=false;
+    setTimeout(function(){selectTimeout=true;},10);
+    if(aDown){
+        selected.push(id);
+    }else{
+        $("circle").css("fill","white");
+        selected[0]=id;
+    }
+    $("#"+id).css("fill","#ADD8E6");
+}
+
 function constructPage(){
-    $("text,rect,circle,.relation,.object").remove();
+    $("text,rect,circle,.relation_container").remove();
     headings=[];
     branches=[];
     bubbles=[];
@@ -32,40 +47,47 @@ function constructPage(){
     //creation of heading structure
     for(var relation in nodeData["relations"]){
         var nodes=nodeData["relations"][relation];
-        headings.push(new heading("noid",relation,"relation"));
+        var relDiv=div.append("div").attr("class","relation_container");
+        relDiv.append("div").text(relation).attr("class","relation_name");
         for(var i in nodes){
-            headings.push(new heading(nodes[i]["id"],function(){
+            objDiv=relDiv.append("div").attr("class","object");
+            var text=function(){
                 //return _ if object has no relationships, indicates to user that this node needs naming
-                    if(!Object.keys(nodes[i]["relations"])[0]){
-                        return "_";
-                    }else{
-                        return Object.keys(nodes[i]["relations"])[0];
-                    }
+                    if(!Object.keys(nodes[i]["relations"])[0]){return "_";}
+                    else{return Object.keys(nodes[i]["relations"])[0];};
                 }
-                ,"object"));
+            objDiv.append("div").attr("id",nodes[i]["relid"]).attr("class","object_relation").text("\u21d2");
+            objDiv.append("div").attr("id",nodes[i]["id"]).attr("class","object_name").text(text);
         }
     }
 
-    //creation of branch structure
+    addBubbleDisplay("contains");
 
-
-    //creation of bubble structure
-    addBubble(nodeData,0,0,250,1);
-    for(var i=branches.length;i>0;i--){
-        branches[i-1].draw();
-    }
-
-    $("text,div,circle").click(function(){
-        if(mode=="navigate"){
+    $("circle,.object_name,.object_relation").click(function(){
+        if(mode=="view"){
             var id=parseInt(this.id);
             if(Number.isInteger(id)){
-                query("get",id);
+                if(this.class=="object_relation"){
+                    query("get_rel",id);
+                }else{
+                    query("get",id);
+                }
+            }
+            if(mode=="edit"&&selectTimeout){
+                selectNode(this.id);
             }
         }
     });
 }
 
-function addBubble(array,myX,myY,myR,level){
+function addBubbleDisplay(relation){
+    addBubble(nodeData,0,0,250,1,relation);
+    for(var i=branches.length;i>0;i--){
+        branches[i-1].draw();
+    }
+}
+
+function addBubble(array,myX,myY,myR,level,relation){
     bubbles.push(new bubble(array["id"],myX,myY,myR));
 
     if(level==2){
@@ -85,14 +107,14 @@ function addBubble(array,myX,myY,myR,level){
     }
 
     if(array["relations"]["contains"]){
-        var n=array["relations"]["contains"].length;
+        var n=array["relations"][relation].length;
         var cR=relR(myR,n);
         for(var i=0;i<n;i++){
             var cT=(Math.PI/2-Math.PI/n)+i*2*Math.PI/n;
             var cX=(myR-cR)*Math.cos(cT)+myX;
             var cY=(myR-cR)*Math.sin(cT)+myY;
             if(n==1){cX=myX;cY=myY;cR=myR/golden;}
-            addBubble(array["relations"]["contains"][i],cX,cY,cR,level+1);
+            addBubble(array["relations"][relation][i],cX,cY,cR,level+1,relation);
         }
     }
 }
@@ -157,12 +179,6 @@ class branch{
             var path="m "+x1+" "+y1+" c "+(x-x1-arb)+" 0 "+arb+" "+(y-y1)+" "+(x-x1)+" "+(y-y1);
             this.lineNode=svg.append("path").attr("d",path).attr("class","link");
         }
-    }
-}
-
-class heading{
-    constructor(id,text,className){
-        this.node=div.append("div").attr("id",id).text(text).attr("class",className);
     }
 }
 
