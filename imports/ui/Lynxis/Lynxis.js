9d4b17dc49dacd0a851f688d18d54c7d7ef0bfe7
Keller Jordan <kellerjordan1998@gmail.com>
2016-10-04 15:15:50 -0700

    debugged TextNode component, added title display

--- imports/ui/Lynxis/Lynxis.js
+++ imports/ui/Lynxis/Lynxis.js
@@ -29,21 +29,22 @@ const App = React.createClass({
 	render() {
 		let focus = this.props.focus, nodes = this.props.nodes;
 		return (
-			<div id="container">
-				<div className="row center-align" style={{ paddingTop: '20px' }}>
-					<TextNode id={focus._id} text={focus.name} />
+			<div className="container">
+				<div className="row" style={{ paddingTop: '20px' }}>
+					<div className="col s12 focus">
+						<TextNode id={focus._id} text={focus.name} />
+					</div>
 				</div>
 				<ul className="collection">
 					{nodes.map(node => {
-						let id = node._id;
 						return (
-							<li key={id} className="collection-item">
-								<TextNode id={id} text={node.name} />
+							<li key={node._id} className="collection-item">
+								<TextNode id={node._id} text={node.name} />
 							</li>
 						);
 					})}
-					<li className="collection-item" id="addNode">Add a Node</li>
 				</ul>
+				<a className="waves-effect waves-light btn" id="addNode">Add a Node</a>
 				<div className="description">
 					abc
 				</div>
@@ -61,8 +62,6 @@ const TextNode = React.createClass({
 		}
 	},
 
-	componentWillReceiveProps: function(nextProps) { this.setState({ hovered: nextProps.hovered })},
-
 	handleChange(event) {
 		let text = event.target.value;
 		this.setState({ text });
@@ -71,40 +70,34 @@ const TextNode = React.createClass({
 
 	handleHover(hover) { this.setState({ hovered: hover }) },
 
-	handleFocus(focus) {
-		this.setState({ focused: focus });
-	},
+	handleFocus(focus) { this.setState({ focused: focus }) },
 
 	render() {
 		let text = this.state.text,
-			divVisible = this.state.hovered || this.state.focused ? "hidden" : "visible",
-			textareaVisible = (divVisible == "visible") ? "hidden" : "visible",
-			html;
-		try {
-			html = katex.renderToString(text);
-		} catch(err) {
-			if(err) html = text;
-		}
-		return (
-			<div onMouseEnter={this.handleHover.bind(this, true)} onMouseLeave={this.handleHover.bind(this, false)} >
+			html,
+			TextNode;
+		try { html = katex.renderToString(text) } catch(err) { if(err) html = text }
+
+		if(this.state.hovered || this.state.focused) {
+			TextNode = (
 				<textarea
 					onFocus={this.handleFocus.bind(this, true)}
 					onChange={this.handleChange}
 					onBlur={this.handleFocus.bind(this, false)}
 					value={text}
-					style={{
-						visibility: textareaVisible,
-						zIndex: 1000,
-						fontSize: '14.67px',
-						lineHeight: '20px',
-						height: '20px'
-					}}
 				/>
+			)
+		} else {
+			TextNode = (
 				<div
-				style={{visibility: divVisible}}
-				className="TextNode"
-				dangerouslySetInnerHTML={{__html: html}}
+					dangerouslySetInnerHTML={{__html: html}}
 				/>
+			)
+		}
+
+		return (
+			<div className="TextNode" onMouseEnter={this.handleHover.bind(this, true)} onMouseLeave={this.handleHover.bind(this, false)} >
+				{TextNode}
 			</div>
 		);
 	}
