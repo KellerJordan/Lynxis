267e00fb45139498f743400e732f45db01b9d433
Keller Jordan <kellerjordan1998@gmail.com>
2016-10-06 18:54:32 -0700

    moved all vars to within react, improved method call orders, data fetching

--- imports/ui/Lynxis/Lynxis.js
+++ imports/ui/Lynxis/Lynxis.js
@@ -6,35 +6,48 @@ import './Lynxis.html';
 import React from 'react';
 import { render } from 'react-dom';
 
-let root_id, focus, mode = 'view';
-root_id = '';
-// focus = 'RTsTCFBnoHjDNn7Y4';
-
-Meteor.autorun(() => {
-
-	Meteor.subscribe('nodes.all');
-	Meteor.subscribe('links.all');
-
-	renderApp();
-});
 
 $(document).ready(() => { $('.modal-trigger').leanModal() });
 
+Meteor.startup(() => { render(<App root="" />, document.getElementById('render-target')) });
 
-function renderApp() {
-	Meteor.call('getNodes', { root: root_id, text: 'contains' }, (error, result) => {
-		render(<App root={result.root} nodes={result.nodes} />, document.getElementById('render-target'));
-	});
-}
+Meteor.autorun(() => {
+	Meteor.subscribe('nodes.all');
+	Meteor.subscribe('links.all');
+});
 
 const App = React.createClass({
+	getInitialState() {
+		let id = this.props.root;
+		return {
+			id,
+			root: {},
+			nodes: [],
+			focus: id,
+			mode: 'view',
+			loading: true
+		}
+	},
+
+	getData(id) {
+		this.setState({ loading: true })
+		Meteor.call(
+			'getNodes',
+			{ root: id, text: 'contains' },
+			(error, result) => {
+				this.setState({ id, root: result.root, nodes: result.nodes, loading: false })
+			}
+		);
+	},
+
 	componentWillMount() {
+		this.getData(this.state.id);
+
 		$(document).on('keydown', (e) => {
-			if(mode == 'view') {
+			if(this.state.mode == 'view') {
 				switch(e.which) {
 					case 81: // q
-					root_id = '';
-					renderApp();
+					this.getData('');
 					break;
 					case 87: // w
 					// go back by one
@@ -43,8 +56,7 @@ const App = React.createClass({
 					// ?
 					break;
 					case 82: // r
-					root_id = focus;
-					renderApp();
+					this.getData(this.state.focus);
 					break;
 					case 65: // a
 					// ?
@@ -55,43 +67,71 @@ const App = React.createClass({
 					case 68: // d
 					e.preventDefault();
 					if(e.ctrlKey) {
-						deleteNode.call({ id: focus });
+						Meteor.call(
+							'deleteNode',
+							{ id: this.state.focus },
+							() => { this.getData(this.state.id) }
+						);
 					}
 					break;
 					case 70: // f
-					Meteor.call('insertNode', {}, (error, id) => {
-						if(root_id && !error) Meteor.call('upsertLink', { subject: root_id, object: id, text: 'contains' })
-					})
+					Meteor.call(
+						'insertNode',
+						{},
+						(error, id) => {
+							if(this.state.id && !error) {
+								Meteor.call(
+									'upsertLink',
+									{ subject: this.state.id, object: id, text: 'contains' },
+									() => {
+										this.getData(this.state.id);
+									}
+								);
+							} else {
+								this.getData(this.state.id);
+							}
+						}
+					);
 					break;
 				}
 			}
 			if(e.which == 9) {
 				e.preventDefault();
-				mode = (mode == 'edit') ? 'view' : 'edit';
-				renderApp();
+				this.setState({ mode: (this.state.mode == 'edit') ? 'view' : 'edit' });
 			}
 		});
 	},
 
-	componentWillUnmount() {
-		$(document).off('keydown');
-	},
+	componentWillUnmount() { $(document).off('keydown') },
+
+	handleMouseDown(focus) { if(this.state.mode == 'view') this.setState({ focus }) },
 
 	render() {
-		let root = this.props.root, nodes = this.props.nodes;
+		if(this.state.loading) return <div>Loading...</div>;
+
 		return (
 			<div className="container">
 				<div className="row" style={{ paddingTop: '20px' }}>
-					<div className="col s12 root">
-						<TextNode id={root._id} text={root.name} />
+					<div className="col s12 root" onMouseDown={this.handleMouseDown.bind(this, this.state.id)}>
+						<TextNode
+							id={this.state.root._id}
+							text={this.state.root.name}
+							mode={this.state.mode}
+							className={`TextNode ${(this.state.focus == this.state.root._id && this.state.mode == 'view') ? 'focus' : ''}`}
+						/>
 					</div>
 				</div>
 				<div className="row">
 					<ul className="collection">
-						{nodes.map(node => {
+						{this.state.nodes.map(node => {
 							return (
-								<li key={node._id} className="collection-item">
-									<TextNode id={node._id} text={node.name} />
+								<li key={node._id} className="collection-item" onMouseDown={this.handleMouseDown.bind(this, node._id)}>
+									<TextNode
+										id={node._id}
+										text={node.name}
+										mode={this.state.mode}
+										className={`TextNode ${(this.state.focus == node._id && this.state.mode == 'view') ? 'focus' : ''}`}
+									/>
 								</li>
 							);
 						})}
@@ -104,7 +144,7 @@ const App = React.createClass({
 				</div>
 				<div className="row">
 					<div>
-						{`Mode: ${mode == 'view' ? 'traversal' : 'manipulation'}`}
+						{`Mode: ${this.state.mode == 'view' ? 'traversal' : 'manipulation'}`}
 					</div>
 				</div>
 			</div>
@@ -115,21 +155,10 @@ const App = React.createClass({
 const TextNode = React.createClass({
 	getInitialState() { return { text: this.props.text } },
 
-	componentWillReceiveProps({ id, text }) {
-		if(id != this.props.id) this.setState({ text });
-	},
-
 	handleChange(e) {
 		let text = e.target.value;
 		this.setState({ text });
 		upsertLink.call({ subject: this.props.id, object: name_id, text });
-	},
-
-	handleClick() {
-		if(mode == 'view') {
-			focus = this.props.id;
-			renderApp();
-		}
 	},
 
 	render() {
@@ -142,11 +171,11 @@ const TextNode = React.createClass({
 			html = text;
 		}
 
-		if(mode == 'edit') TextNode = <textarea onChange={this.handleChange} value={text} />;
+		if(this.props.mode == 'edit') TextNode = <textarea onChange={this.handleChange} value={text} />;
 		else TextNode = <div dangerouslySetInnerHTML={{__html: html}} />;
 
 		return (
-			<div className={`TextNode ${(focus == this.props.id && mode == 'view') ? 'focus' : ''}`} onClick={this.handleClick} >
+			<div className={this.props.className} >
 				{TextNode}
 			</div>
 		);
